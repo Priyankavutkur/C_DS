01b104fefbc65129edaea0504aad8b696ae92347
priyanka <priyankavutkur6@gamil.com>
2022-10-03 20:38:41 +0530

    C_data_types

---
+++ c_programing/Data_type_size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("sizeof(int)=%d\n",sizeof(int));
+    printf("sizeof(char)=%d\n",sizeof(char));
+    printf("sizeof(double)=%d\n",sizeof(double));
+    printf("sizeof(float)=%d\n",sizeof(float));
+
+    return 0;
+}
