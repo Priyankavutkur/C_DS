fbdf6a68e8a9e2876e9aa54a6cb498bf3dadcf6a
srujan <srujan352002@gmail.com>
2022-10-12 12:21:36 +0530

    Array

--- Array/1D_array.c
+++ Array/1D_array.c
@@ -2,13 +2,15 @@
 #define size 10
 void main()
 {
-	int arr[size],i;
+	int i,size1;
+	scanf("%d",&size1);
+	int arr[size1];
 	printf("enter array elements");
-	for(i=0;i<size;i++)
+	for(i=0;i<size1;i++)
 	{
 		scanf("%d",&arr[i]);
 	}
-	for(i=0;i<size;i++)
+	for(i=0;i<size1;i++)
 	{
 		printf("%d ",arr[i]);
 	}
